b9f26c9e7d9a1ac27c3a12253cad614ede7f026f
Jonathan Brossard <endrazine@gmail.com>
2021-01-26 09:18:41 +0100

    Added initial Toucan System website.

---
+++ php/process-form.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_REQUEST['firstname'],$_REQUEST['email'])) {
+      
+    $firstname = $_REQUEST['firstname'];
+	$lastname = $_REQUEST['lastname'];
+    $email = $_REQUEST['email'];
+    $message = $_REQUEST['message'];
+      
+    // Set your email address where you want to receive emails. 
+    $to = 'youremail@mail.com';
+      
+    $subject = 'Contact Request From Website';
+    $headers = "From: ".$firstname." <".$email."> \r\n";
+      
+    $send_email = mail($to,$subject,$message,$headers);
+      
+    echo ($send_email) ? 'success' : 'error';
+      
+}
+?>
